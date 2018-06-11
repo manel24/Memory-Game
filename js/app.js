11a71ba1ff14d097c7c2615728fba4f24485c2ad
ManelCH <manelcharouni@think-it.io>
2018-06-11 13:49:52 +0100

    bug fixing

--- js/app.js
+++ js/app.js
@@ -22,11 +22,11 @@ let cards = [
 ];
 let open_cards = [];
 
-let game_over = false
+let game_over = false;
 
-let moves = 0
+let moves = 0;
 
-let stars = 0
+let stars = 0;
 /*
  * Display the cards on the page
  *   - loop through each card and create its HTML
@@ -36,7 +36,7 @@ let stars = 0
 
 function initGame() {
   cards = shuffle(cards);
-  cards.forEach(function (card, index) {
+  cards.forEach(function(card, index) {
     const c = document.createElement("li");
     c.innerHTML = `<i class="fa ${card}"></i>`;
     c.classList.add("card");
@@ -45,6 +45,7 @@ function initGame() {
 }
 
 function shuffle(array) {
+  document.querySelector(".moves").textContent = moves;
   var currentIndex = array.length,
     temporaryValue,
     randomIndex;
@@ -60,83 +61,93 @@ function shuffle(array) {
   return array;
 }
 
-function displayCard(e) {
-  e.target.classList.add("show");
-  e.target.classList.add("open");
+function displayCard(card) {
+  card.classList.add("show");
 }
 
 function clearBoard() {
-  document.querySelectorAll('.card.open').forEach(function (card) {
-    card.classList.remove('show')
-    card.classList.remove('open')
-    open_cards.pop()
-    open_cards.pop()
-    console.log('open cards are now : ', open_cards)
-  })
+  if (open_cards.length % 2 == 0) {
+    document.querySelectorAll(".card.show").forEach(function(card) {
+      card.classList.remove("show");
+      open_cards.pop();
+    });
+    console.log("open cards are now : ", open_cards);
+  }
 }
 
-function setAsOpenCard(e) {
-  open_cards.push(e.target.childNodes[0].classList[1]) // Expl : adds "fa-diamond" to open_cards
+function setAsOpenCard(card) {
+  open_cards.push(card.childNodes[0].classList[1]); // Expl : adds "fa-diamond" to open_cards
 }
 
+//TODO fix click events issue when game is restarted
 function restartGame() {
-  document.querySelector(".deck").innerHTML = ''
-  open_cards = []
-  moves = 0
-  game_over = false
-  initGame()
+  moves = 0;
+  game_over = false;
+  document.querySelector(".deck").innerHTML = ""; //empty the game board
+  initGame();
+  open_cards = [];
 }
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
 
+function matchFound() {
+  console.log("match found");
+  document.querySelectorAll(`.card.show`).forEach(function(card) {
+    console.log(card);
+    card.classList.remove("show");
+    card.classList.add("open");
+    card.classList.add("match");
+  });
+  document.querySelector(".stars").innerHTML +=
+    '<li> <i class = "fa fa-star" > </i></li>';
+  stars = stars + 1;
+  console.log("stars=", stars);
+  if (open_cards.length === cards.length) {
+    game_over = true;
+  }
+}
+//TODO fix simultaneous clicks > 2 issue (disabling click events after 2 open cards)
 function playGame() {
-  document.querySelectorAll(".card").forEach(function (card) {
-    card.addEventListener("click", function (e) {
-      displayCard(e);
-      setAsOpenCard(e);
+  document.querySelectorAll(".card").forEach(function(card) {
+    card.addEventListener("click", function(e) {
+      card = e.target; //current card
+      displayCard(card);
+      setAsOpenCard(card);
+      moves = moves + 1; //increment moves
+      document.querySelector(".moves").textContent = moves;
       if (open_cards.length > 1) {
-        console.log('current class:  ', e.target.childNodes[0].classList[1])
-        console.log('current open cards  ', open_cards)
-        if (open_cards[open_cards.length - 1] === open_cards[open_cards.length - 2]) {
-          console.log('match found')
-          document.querySelectorAll('.card.open').forEach(function (card) {
-            card.classList.remove('open')
-            card.classList.remove('show')
-            card.classList.add('match')
-
-          })
-          document.querySelector('.stars').innerHTML +=
-            '<li> <i class = "fa fa-star" > </i></li>'
-          stars = stars + 1
-          console.log('stars=',stars)
+        console.log("current class:  ", card.childNodes[0].classList[1]);
+        console.log("current open cards  ", open_cards);
+        //match found
+        if (
+          open_cards[open_cards.length - 1] ===
+          open_cards[open_cards.length - 2]
+        ) {
+          matchFound();
         }
       }
-      moves = moves + 1
-      document.querySelector('.moves').textContent = moves
-      if (stars === 8) {
-        alert(`Well done ! you won with ${moves} moves.`)
+      //ALl cards have been matched
+      if (open_cards.length === cards.length) {
+        game_over = true;
+      }
+      //GAME OVER
+      //TODO fix issue of having last card not turning "matched" only after the alert box pops up
+      if (game_over) {
+        alert(`Well done ! you won with ${moves} moves.`);
       }
     });
-    card.addEventListener("mouseout", function (e) {
-      if ((open_cards.length >= 2) && (open_cards.length % 2 == 0)) {
-        setTimeout(function () {
-          clearBoard(open_cards)
-        }, 1000);
-
+    card.addEventListener("mouseout", function(e) {
+      console.log("mouse out event");
+      if (open_cards.length >= 2 && open_cards.length % 2 == 0) {
+        setTimeout(function() {
+          //Clear non matching cards from the board
+          clearBoard(open_cards);
+        }, 2000);
       }
     });
-  })
-  document.querySelector('.restart').addEventListener('click', function (e) {
-    restartGame()
-  })
+  });
+  //restart GAME
+  document.querySelector(".restart").addEventListener("click", function(e) {
+    restartGame();
+  });
 }
 
 /*main program*/
